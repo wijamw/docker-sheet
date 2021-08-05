85980d8ff935c13c83204927cda8b1b64f061d0d
Moh. Wijayanto <wija.mw17@gmail.com>
2021-08-05 15:02:46 +0700

    Fix Missing Tab After Image

--- docker-ps-format.sh
+++ docker-ps-format.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Export format to system enviroment
-echo 'export PSFORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n"' >> ~/.bashrc
+echo 'export PSFORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE\t{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n"' >> ~/.bashrc
 echo '>>> Reload Bash Shell'
 exec "$BASH"
 # Example
