e98a035109d74910bcb289d42af0436289338f95
Moh. Wijayanto <wija.mw17@gmail.com>
2021-08-05 10:52:42 +0700

    Change from export to /etc/environement

--- docker-ps-format.sh
+++ docker-ps-format.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Export format to system enviroment
-export PSFROMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n"
+echo "export PSFORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n"" >> /etc/environment
 
 # Example
 # docker ps --format=$PSFORMAT
