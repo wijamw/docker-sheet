889caabbd36009503dd3fe26f4c772a64294745c
wijamw <wija.mw17@gmail.com>
2021-08-05 11:05:50 +0700

    Fix

--- docker-ps-format.sh
+++ docker-ps-format.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Export format to system enviroment
-echo "export PSFORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n"" >> /etc/environment
-
+echo 'export PSFORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n"' >> ~/.bashrc
+echo '>>> Reload Bash Shell'
+exec "$BASH"
 # Example
 # docker ps --format=$PSFORMAT
 
